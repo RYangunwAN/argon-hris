e129859030b41964f766fd7ae5d9afa83745116e
RYangunawAN <ryangunawan.we@gmail.com>
2024-08-28 12:20:56 +0700

    init commit

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/api', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
