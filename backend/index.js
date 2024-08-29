9ea5f50178c078e563f803b48dd28291dc9c6f52
RYangunawAN <ryangunawan.we@gmail.com>
2024-08-29 10:56:52 +0700

    init database connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cors = require('cors')
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-  res.send('Hello from Express!');
+  res.send('Argon HRIS backend');
 });
 
 app.listen(port, () => {
